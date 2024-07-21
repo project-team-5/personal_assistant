d1e8997d5881d4b5ee061375a48ce20bf20bb170
XenKos <kosarevskayakseniia@gmail.com>
2024-07-21 16:04:25 +0200

    Added features to notes

---
+++ core/notes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.NoteListView.as_view(), name='note-list'),
+    path('create/', views.NoteCreateView.as_view(), name='note-create'),
+    path('<int:pk>/', views.NoteDetailView.as_view(), name='note-detail'),
+    path('<int:pk>/update/', views.NoteUpdateView.as_view(), name='note-update'),
+    path('<int:pk>/delete/', views.NoteDeleteView.as_view(), name='note-delete'),
+    path('search/', views.NoteSearchView.as_view(), name='note-search'),
+]
+
