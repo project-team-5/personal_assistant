e545f134a319a512a313c2640f371e0435e9fd2a
XenKos <kosarevskayakseniia@gmail.com>
2024-07-24 23:29:28 +0200

    Added features to notes (#1)
    approved/ features to notes added to base code.

--- core/notes/models.py
+++ core/notes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='notes')
+
+    def __str__(self):
+        return self.title
+
+
+        
