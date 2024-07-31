237f4a657836e12524cc8222d4428febe2ca5f55
Valentyna Rudenko <valentino4ka2009@gmail.com>
2024-07-31 18:21:20 +0300

    hosts

--- core/core/settings.py
+++ core/core/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['personal-assistant-eagles.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
