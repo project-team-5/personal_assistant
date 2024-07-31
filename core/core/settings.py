98b5dc417d71fa6b5e1f984b26d3cddce0065a78
Valentyna Rudenko <valentino4ka2009@gmail.com>
2024-07-31 16:53:03 +0300

    Debug

--- core/core/settings.py
+++ core/core/settings.py
@@ -33,7 +33,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
